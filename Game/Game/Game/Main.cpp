be2f9f233c4dec1b2c28c8982514520b7bb32380
ThanThor1 <vuxuandungnb2k5@gmail.com>
2024-03-15 16:26:23 +0700

    chaoban

--- Game/Game/Game/Main.cpp
+++ Game/Game/Game/Main.cpp
@@ -23,7 +23,7 @@ int main(int argc, char* args[]) {
 			}
 			checkEvent(e);
 		}
-		cout << "lol";
+		cout << "chaobannhe";
 		Wave1();
 		SDL_RenderClear(gRenderer);
 		SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
