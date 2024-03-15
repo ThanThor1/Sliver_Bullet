3c3543b4c55141700aba76c976ef245a287abf81
ThanThor1 <vuxuandungnb2k5@gmail.com>
2024-03-15 16:09:59 +0700

    haha

---
+++ Game/Game/Game/Event.h
@@ -0,0 +1,6 @@
+#pragma once
+#ifndef EVENT
+#define EVENT
+#include "Library.h"
+void checkEvent(SDL_Event e);
+#endif
---
+++ Game/Game/Game/Object.h
@@ -0,0 +1,35 @@
+#pragma once
+#pragma once
+#ifndef OBJECT
+#define OBJECT
+#include "Library.h"
+class Object
+{
+public:
+	bool exist = false;
+	//
+	double start_x;
+	double start_y;
+	// dia chi trên màn hình lúc render ra
+	int O_x;
+	int O_y;
+	// dia chi diem bat dau (chua can dùng)
+	//chieu ngang và doc
+	int O_Width = 0;
+	int O_Height = 0;
+	//Texture
+	SDL_Texture* O_Texture = NULL;
+	double slope = 0;
+	double angle = 0;
+	bool loadFromFile(string path);
+	void setColor(Uint8 red, Uint8 green, Uint8 blue);
+	void setBlendMode(SDL_BlendMode blending);
+	void setAlpha(Uint8 alpha);
+	void render(int x, int y, SDL_Rect* clip = NULL,
+		double angle = 0.0, SDL_Point* center = NULL,
+		SDL_RendererFlip flip = SDL_FLIP_NONE);
+	Object();
+	~Object();
+	void free();
+};
+#endif
